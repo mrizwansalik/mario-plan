e0be4713c5e55f106208358e743c8e1718395fa3
MuhammadRizwanSalik <47275249+mrizwansalik@users.noreply.github.com>
2020-10-27 11:34:17 +0500

    final buid

---
+++ dist/build/precache-manifest.16cbffa0fdcfe63238a7667169ab7af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363c5fd981b745580be0e88a62bf9c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da7bedfb54754ad98cf",
+    "url": "/static/css/main.7929e5bf.chunk.css"
+  },
+  {
+    "revision": "0ead5811b7cc38c8d2a1",
+    "url": "/static/js/2.775650fe.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.775650fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da7bedfb54754ad98cf",
+    "url": "/static/js/main.cd708262.chunk.js"
+  },
+  {
+    "revision": "3839eb390f78503f8246",
+    "url": "/static/js/runtime-main.daf3d22f.js"
+  }
+]);
